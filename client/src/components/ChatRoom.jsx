6c546e58d09e747e3a5884ed56a114a55ab23c8b
eezlogicxarvind <159049532+voidloopxarvin@users.noreply.github.com>
2026-01-10 18:35:45 +0530

    Update ChatRoom.jsx

--- client/src/components/ChatRoom.jsx
+++ client/src/components/ChatRoom.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { Send, Loader2, Users, CheckCircle } from 'lucide-react';
-import { useSocket } from '../../context/SocketContext';
+import { useSocket } from '../context/SocketContext';
 import { getApiUrl } from '../../utils/apiUrl';
 
 const ChatRoom = () => {
